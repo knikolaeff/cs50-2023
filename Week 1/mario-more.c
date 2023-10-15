1790395291b550a143afbf04046f86e3bf126f5e
Kirill Nikolaef <kirillnikolaefff@gmail.com>
2023-10-15 17:16:36 +0100

    Formatting

--- Week 1/mario-more.c
+++ Week 1/mario-more.c
@@ -3,46 +3,43 @@
 
 void print_pyramid(int);
 
-int main(void)
-{
-    char buf[512];
-    int height;
+int main(void) {
+  char buf[512];
+  int height;
 
-    do
-    {
-        printf("Height: ");
-        fgets(buf, sizeof buf, stdin);
-        height = atoi(buf);
-    }
-    while (height < 1 || height > 16);
+  do {
+    printf("Height: ");
+    fgets(buf, sizeof buf, stdin);
+    height = atoi(buf);
+  } while (height < 1 || height > 16);
 
-    print_pyramid(height);    
+  print_pyramid(height);
 }
 
 void print_pyramid(int height) {
-int spaces = height - 1;
+  int spaces = height - 1;
 
-    for (int i = 0; i < height; i++)
-    {
-
-        for (int j = 0; j < spaces; j++)
-        {
-            printf(" ");
-        }
-
-        for (int hashes = height - spaces; hashes > 0; hashes--)
-        {
-            printf("#");
-        }
-
-        printf("  ");
-
-        for (int hashes = height - spaces; hashes > 0; hashes--)
-        {
-            printf("#");
-        }
-
-        printf("\n");
-        spaces -= 1;
+  for (int i = 0; i < height; i++) {
+    // prints spaces before blocks
+    for (int j = 0; j < spaces; j++) {
+      printf(" ");
     }
+
+    // prints left side of the pyramid
+    for (int hashes = height - spaces; hashes > 0; hashes--) {
+      printf("#");
+    }
+
+    // prints two-blocks wide space between the left and right side of the
+    // pyramid
+    printf("  ");
+
+    // prints right side of the pyramid
+    for (int hashes = height - spaces; hashes > 0; hashes--) {
+      printf("#");
+    }
+
+    printf("\n");
+    spaces -= 1;
+  }
 }
