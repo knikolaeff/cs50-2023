9e045a601c15cfe0b53bd60bd5771fa79214552d
Kirill Nikolaef <kirillnikolaefff@gmail.com>
2023-10-15 17:25:36 +0100

    Fixed formatting using style50

--- Week 1/mario-more.c
+++ Week 1/mario-more.c
@@ -3,43 +3,51 @@
 
 void print_pyramid(int);
 
-int main(void) {
-  char buf[512];
-  int height;
+int main(void)
+{
+    char buf[512];
+    int height;
 
-  do {
-    printf("Height: ");
-    fgets(buf, sizeof buf, stdin);
-    height = atoi(buf);
-  } while (height < 1 || height > 16);
+    do
+    {
+        printf("Height: ");
+        fgets(buf, sizeof buf, stdin);
+        height = atoi(buf);
+    }
+    while (height < 1 || height > 16);
 
-  print_pyramid(height);
+    print_pyramid(height);
 }
 
-void print_pyramid(int height) {
-  int spaces = height - 1;
+void print_pyramid(int height)
+{
+    int spaces = height - 1;
 
-  for (int i = 0; i < height; i++) {
-    // prints spaces before blocks
-    for (int j = 0; j < spaces; j++) {
-      printf(" ");
+    for (int i = 0; i < height; i++)
+    {
+        // prints spaces before blocks
+        for (int j = 0; j < spaces; j++)
+        {
+            printf(" ");
+        }
+
+        // prints left side of the pyramid
+        for (int hashes = height - spaces; hashes > 0; hashes--)
+        {
+            printf("#");
+        }
+
+        // prints two-blocks wide space between the left and right side of the
+        // pyramid
+        printf("  ");
+
+        // prints right side of the pyramid
+        for (int hashes = height - spaces; hashes > 0; hashes--)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+        spaces -= 1;
     }
-
-    // prints left side of the pyramid
-    for (int hashes = height - spaces; hashes > 0; hashes--) {
-      printf("#");
-    }
-
-    // prints two-blocks wide space between the left and right side of the
-    // pyramid
-    printf("  ");
-
-    // prints right side of the pyramid
-    for (int hashes = height - spaces; hashes > 0; hashes--) {
-      printf("#");
-    }
-
-    printf("\n");
-    spaces -= 1;
-  }
 }
