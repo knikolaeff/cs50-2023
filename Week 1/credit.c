796e8088a5bed080176c050a4fd59dad13a35d57
Kirill Nikolaef <kirillnikolaefff@gmail.com>
2023-10-15 22:10:31 +0100

    Finished the overall logic, but still need to fix bugs

--- Week 1/credit.c
+++ Week 1/credit.c
@@ -1,5 +1,103 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void check_card(long);
+int find_card_type(long);
 
 int main(void) {
-    
+    char buf[16];
+    long card_number;
+
+    // prompt for a number and check if the value can be converted to long
+    do
+    {
+        printf("Number: ");
+        fgets(buf, sizeof buf, stdin);
+        card_number = atol(buf);
+    }
+    while (card_number < 1);
+
+    check_card(card_number);
+
+    return(0);
 }
+
+// Luhn's algorithm
+void check_card(long card_number)
+{
+    int first_sum = 0;
+    int second_sum = 0;
+
+    while (card_number > 0)
+    {
+        // first/second from the back
+        int first_digit = card_number % 10;
+        int second_digit = (card_number % 10) % 10;
+
+        // removes two digits from the back
+        card_number /= 100;
+
+        first_sum += first_digit;
+
+        // slices big numbers (>10) into products of the number
+        if (second_digit * 2 > 9)
+        {
+            second_sum += second_digit * 2 / 10;
+            second_sum += second_digit * 2 % 10;
+        }  
+
+        else 
+        {
+            second_sum += second_digit * 2;
+        }
+    }
+
+    if ((first_sum + second_sum) % 10 != 0)
+    {
+        printf("INVALID");
+        exit(0);
+    }
+
+    else 
+    {
+        find_card_type(card_number);
+    }
+}
+
+int find_card_type(long card_number)
+    {
+    int card_num_length = 0;
+    while (card_number < 0)
+    {
+        card_number /= 10;
+        card_num_length += 1;
+    }
+
+    int starting_digits = card_number;
+    while (starting_digits > 100)
+    {
+        starting_digits /= 10;
+    }
+
+    // all AMEX cards are 15 digits long and start with 34 or 37
+    if (card_num_length == 15 && (starting_digits == 34 || starting_digits == 37))
+    {
+        printf("AMEX\n");
+        return(0);
+    }
+    // all Mastercard cards are 15 digits long and start with 51-55
+    else if (card_num_length == 16 && (51 <= starting_digits && starting_digits >= 55)) {
+        printf("MASTERCARD\n");
+        return(0);
+    }
+    // all VISA cards are 13 or 16 digits long and start with 4
+    else if ((card_num_length == 13 || card_num_length == 16) && starting_digits / 10 == 4) {
+        printf("VISA\n");
+        return(0);
+    }
+    else 
+    {
+        printf("INVALID\n");
+        return(0);
+    }
+    }
